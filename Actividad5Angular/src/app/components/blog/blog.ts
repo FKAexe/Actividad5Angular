91c8f57ac73a4f2d2d74d75b94d84252c73ec1b9
fkaexe <josemiguelmartinezay@gmail.com>
2025-07-12 04:05:05 +0200

    Modificacion de componentes y clases.

--- Actividad5Angular/src/app/components/blog/blog.ts
+++ Actividad5Angular/src/app/components/blog/blog.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { INoticia } from '../../interfaces/inoticia';
 
 @Component({
   selector: 'app-blog',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './blog.css'
 })
 export class Blog {
+  arrayNoticias: INoticia[] = [
+    {titulo: 'Aviso naranja en 6 comunidades autónomas.', imagen:'https://picsum.photos/id/110/367/267',texto:'Altas temperaturas vienen pronto a estas zonas.',fecha:'10-07-2025'},
+    {titulo: 'Perro rescata a niño de una pozo.', imagen:'https://picsum.photos/id/237/3500/2095',texto:'Locales dicen que esta es una manera usual de actuar para el perro, los historiadores se mantienen divididos.',fecha:'12-07-2025'}
+  ]
 
 }
